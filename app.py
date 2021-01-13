37f881e720858ac34a41226fa3403790e5b1c1ea
Varya Minkina <minkina.vj@gmail.com>
2021-01-13 12:17:27 +0300

    fixed json format and return values

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 import json
 from flask.views import MethodView
-
+from werkzeug.routing import ValidationError
 
 app = Flask(__name__)
 with open("restaurants.json", "r") as read_file:
@@ -12,24 +12,30 @@ restaurants = data['restaurants']
 
 class RestaurantEndpoint(MethodView):
     def get(self):
-        return jsonify({'restaurants': restaurants}), 200
+        return jsonify(restaurants), 200
 
     def post(self):
         content = request.json
-        if content is not None:
-            restaurants[str(len(restaurants))] = content['restaurant_info']
+        if content is None:
+            raise ValidationError
+        restaurants.append(content)
         return jsonify({'restaurants': restaurants}), 201
 
 
 class RestaurantItemEndpoint(MethodView):
     def get(self, restaurant_id):
-        return jsonify({'info': restaurants[str(restaurant_id)]}), 200
+        return jsonify(restaurants[restaurant_id]), 200
 
     def put(self, restaurant_id):
         content = request.json
+        answer = 200
         if content is not None:
-            restaurants[str(restaurant_id)] = content['restaurant_name']
-        return jsonify({'restaurants': restaurants}), 200
+            if len(restaurants) <= restaurant_id:
+                answer = 201
+                restaurants.append(content)
+            else:
+                restaurants[restaurant_id] = content
+        return jsonify(restaurants[restaurant_id]), answer
 
 
 app.add_url_rule("/api/restaurants", view_func=RestaurantEndpoint.as_view("restaurant_api"))
